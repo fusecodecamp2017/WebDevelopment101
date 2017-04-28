b6c337ec2ffd357e922acd150c4c889ece4dbbd3
Keyan Hardman <keyanhardman@gmail.com>
2017-04-28 16:37:59 -0400

    List spotify search results

--- js/spotifySearch.js
+++ js/spotifySearch.js
@@ -11,9 +11,23 @@ $('#spotifySearchForm').submit(function(e) {
     var query = $.ajax({ url: url });
     query.then(function(spotifyResponse) {
         song = spotifyResponse.tracks.items[0].uri;
+
         var queryString = $.param({ uri: song });
 
         var player = '<iframe src="https://embed.spotify.com/?' + queryString + '" width="100%" height="380" frameborder="0" allowtransparency="true"></iframe>'
         spotifyPlayer.html(player);
+
+        listSearchResults(spotifyResponse.tracks.items);
     });
 })
+
+function listSearchResults(tracks) {
+    var searchResults = $('#searchResults');
+    searchResults.html('<div class="searchResult"><b>Not your song? Try one of these<b></div>');
+
+    tracks.forEach(function(track) {
+        artist = track.artists[0].name;
+        name = track.name;
+        searchResults.append('<div class="searchResult">' + artist + ' - ' + name + '</div>')
+    });
+}
